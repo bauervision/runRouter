0cded1ed73326cd82b65bccbbe69f19130a125fe
Mike Bauer <mbauer@avatarpartners.com>
2022-01-06 16:36:38 -0500

    zoom to user location on load

--- MapScreen.js
+++ MapScreen.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { Component, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import { StyleSheet, Text, View, Button, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
-export default () => {
-  return <MapView style={styles.map} showsUserLocation followsUserLocation />;
-};
+export default function MapScreen() {
+  const [location, setLocation] = useState(null);
+  const [mapRegion, setMapRegion] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setMapRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
+
+  return (
+    <MapView
+      style={styles.map}
+      showsUserLocation={true}
+      initialRegion={mapRegion}
+    />
+  );
+}
 
 const styles = StyleSheet.create({
   map: {
